c00c4e5396ccc288e62287a118437c5cfa2fe8c4
Hidetaro7 <hidetaro7@gmail.com>
2023-09-18 23:57:04 +0900

    feat: Headless UI のコンポーネントを追加

--- app.vue
+++ app.vue
@@ -3,6 +3,19 @@
     <div class="prose px-4 py-3">
       <h1 class="text-gray-700">Hello</h1>
       <button class="jumpu-button">button</button>
+      <p>
+        <Modal />
+      </p>
+      <p>
+        <Combobox />
+      </p>
+      <p><Menu /></p>
+      <p>
+        <Listbox />
+      </p>
+      <p>
+        <RadioGroup />
+      </p>
     </div>
   </div>
 </template>
---
+++ components/RadioGroup.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="">
+    <div class="max-w-md">
+      <RadioGroup v-model="selected">
+        <RadioGroupLabel class="sr-only">Server size</RadioGroupLabel>
+        <div class="space-y-2">
+          <RadioGroupOption
+            as="template"
+            v-for="plan in plans"
+            :key="plan.name"
+            :value="plan"
+            v-slot="{ active, checked }"
+          >
+            <div
+              :class="[
+                active
+                  ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-primary-300'
+                  : '',
+                checked ? 'bg-primary-700 text-white ' : 'bg-white ',
+              ]"
+              class="flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none"
+            >
+              <div class="flex w-full items-center justify-between">
+                <div class="flex items-center">
+                  <div class="text-sm">
+                    <RadioGroupLabel
+                      as="p"
+                      :class="checked ? 'text-white' : 'text-gray-900'"
+                      class="font-medium m-0"
+                    >
+                      {{ plan.name }}
+                    </RadioGroupLabel>
+                    <RadioGroupDescription
+                      as="span"
+                      :class="checked ? 'text-primary-100' : 'text-gray-500'"
+                      class="inline"
+                    >
+                      <span> {{ plan.ram }}/{{ plan.cpus }}</span>
+                      <span aria-hidden="true"> &middot; </span>
+                      <span>{{ plan.disk }}</span>
+                    </RadioGroupDescription>
+                  </div>
+                </div>
+                <div v-show="checked" class="shrink-0 text-white">
+                  <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none">
+                    <circle
+                      cx="12"
+                      cy="12"
+                      r="12"
+                      fill="#fff"
+                      fill-opacity="0.2"
+                    />
+                    <path
+                      d="M7 13l3 3 7-7"
+                      stroke="#fff"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </RadioGroupOption>
+        </div>
+      </RadioGroup>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import {
+  RadioGroup,
+  RadioGroupLabel,
+  RadioGroupDescription,
+  RadioGroupOption,
+} from "@headlessui/vue";
+
+const plans = [
+  {
+    name: "Startup",
+    ram: "12GB",
+    cpus: "6 CPUs",
+    disk: "160 GB SSD disk",
+  },
+  {
+    name: "Business",
+    ram: "16GB",
+    cpus: "8 CPUs",
+    disk: "512 GB SSD disk",
+  },
+  {
+    name: "Enterprise",
+    ram: "32GB",
+    cpus: "12 CPUs",
+    disk: "1024 GB SSD disk",
+  },
+];
+
+const selected = ref(plans[0]);
+</script>
