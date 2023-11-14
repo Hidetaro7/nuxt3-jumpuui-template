d08a47a5e0baf37cc19612e2b812e9a3a5da92b4
Hidetaro7 <hidetaro7@gmail.com>
2023-11-14 23:50:07 +0900

    feat: add nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ["@nuxtjs/tailwindcss"],
   tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',
-    configPath: 'tailwind.config',
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config",
     exposeConfig: false,
     exposeLevel: 2,
     config: {},
-    injectPosition: 'first',
+    injectPosition: "first",
     viewer: true,
   },
 });
