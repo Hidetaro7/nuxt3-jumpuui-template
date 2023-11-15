182a35697da17b9ebb80697f8bcc108ef03352b3
Hidetaro7 <hidetaro7@gmail.com>
2023-11-15 10:11:40 +0900

    chore: add commitizen

--- app.vue
+++ app.vue
@@ -177,5 +177,24 @@ const {
 
 onMounted(() => {
   if (isSupported.value) noteShow();
+
+  if (!("Notification" in window)) {
+    // Check if the browser supports notifications
+    alert("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    // Check whether notification permissions have already been granted;
+    // if so, create a notification
+    const notification = new Notification("Hi there!");
+    // …
+  } else if (Notification.permission !== "denied") {
+    // We need to ask the user for permission
+    Notification.requestPermission().then((permission) => {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        const notification = new Notification("Hi there!");
+        // …
+      }
+    });
+  }
 });
 </script>
