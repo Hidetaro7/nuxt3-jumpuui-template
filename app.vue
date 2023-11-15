1425f33b5c2af32532f1dea21c78081324c2e889
Hidetaro7 <hidetaro7@gmail.com>
2023-11-15 10:28:54 +0900

    chore: add commitizen

--- app.vue
+++ app.vue
@@ -177,22 +177,22 @@ const {
 
 onMounted(() => {
   if (isSupported.value) noteShow();
-
+  const noteOption = {
+    body: "経費申請は本日15時までにお願いします。",
+  };
   if (!("Notification" in window)) {
     // Check if the browser supports notifications
     alert("This browser does not support desktop notification");
   } else if (Notification.permission === "granted") {
     // Check whether notification permissions have already been granted;
     // if so, create a notification
-    const notification = new Notification("Hi there!");
+    const notification = new Notification("秋葉秀樹", noteOption);
     // …
   } else if (Notification.permission !== "denied") {
     // We need to ask the user for permission
     Notification.requestPermission().then((permission) => {
-      // If the user accepts, let's create a notification
       if (permission === "granted") {
-        const notification = new Notification("Hi there!");
-        // …
+        const notification = new Notification("秋葉秀樹", noteOption);
       }
     });
   }
